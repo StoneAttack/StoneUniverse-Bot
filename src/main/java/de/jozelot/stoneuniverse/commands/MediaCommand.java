0c25ae2fe529167fa6f2c67096f94e7adb38ae09
jozelot_ <jozelot77@gmail.com>
2026-07-13 18:08:47 +0200

    fix: wrong logger class in MediaCommand.java

--- src/main/java/de/jozelot/stoneuniverse/commands/MediaCommand.java
+++ src/main/java/de/jozelot/stoneuniverse/commands/MediaCommand.java
@@ -12,7 +12,7 @@ import org.slf4j.LoggerFactory;
 public class MediaCommand implements Command {
 
     private final StoneUniverse bot;
-    private static final Logger logger = LoggerFactory.getLogger(IPCommand.class);
+    private static final Logger logger = LoggerFactory.getLogger(MediaCommand.class);
 
     public MediaCommand(StoneUniverse bot) {
         this.bot = bot;
